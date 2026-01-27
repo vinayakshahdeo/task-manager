62ff8bf8dfa290614c58c8f1a46f891463b810f3
vinayakshahdeo <vinushahdeo3333@gmail.com>
2026-01-27 13:12:42 +0530

    Added new dependencies to package-lock.json including dotenv, mongodb, and mongoose, along with related modules and their versions.

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 import 'reflect-metadata';
 import express, { Express, Request, Response } from 'express';
 import { addRoutes } from './config/routes.config';
+import mongoose from 'mongoose';
+import * as dotenv from 'dotenv';
 
 const app: Express = express();
-const PORT = 3000;
+const PORT: number = (process.env.PORT || 3001) as unknown as number;
+
+dotenv.config();
 
 app.get('/', (_req: Request, res: Response) => {
   res.send('Express + TypeScript Server for nodejs default routes');
@@ -11,6 +15,21 @@ app.get('/', (_req: Request, res: Response) => {
 
 addRoutes(app);
 
-app.listen(PORT, '0.0.0.0', () => {
-  console.warn(`Server running on port ${PORT}`);
-});
+async function bootstrap() {
+  const dbString = process.env.DATABASE_CONNECTION_STRING;
+  if (!dbString) {
+    throw new Error('Cannot read environment variables');
+  }
+  try {
+    await mongoose.connect(dbString, { dbName: process.env.DATABASE_NAME });
+    console.warn('Connected to Mongodb');
+    app.listen(PORT, '0.0.0.0', () => {
+      console.warn(`Server running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+}
+
+bootstrap();
