2159b864aca5718d8663dac03bb5021b23a197e8
vinayakshahdeo <vinushahdeo3333@gmail.com>
2026-02-03 16:27:49 +0530

    Fix typo in import name for GetTaskProvider in container configuration.
    The diff corrects a typo in the import statement for the GetTaskProvider
    in the container configuration file. The incorrect name was updated
    to match the actual exported name of the provider.
    
    - The typo in the import statement was fixed from `GetTaskProvider` to
      `GetTasksProvider`.
    - No functional changes or side effects are expected from this fix.

--- src/config/container.ts
+++ src/config/container.ts
@@ -4,12 +4,12 @@ import { TasksRouter } from "../tasks/tasks.router";
 import { UserController } from "../user/user.controller";
 import { TaskService } from "../tasks/service/task.service";
 import { UpdateTaskProvider } from "../tasks/provider/task.provider";
-import { GetTaskProvider } from "../tasks/provider/getTask.provider";
+import { GetTasksProvider } from "../tasks/provider/getTask.provider";
 
 export const container: Container = new Container();
 container.bind(TasksController).toSelf().inTransientScope();
 container.bind(TasksRouter).toSelf().inTransientScope();
 container.bind(TaskService).toSelf().inTransientScope();
-container.bind(GetTaskProvider).toSelf().inTransientScope();
+container.bind(GetTasksProvider).toSelf().inTransientScope();
 container.bind(UpdateTaskProvider).toSelf().inTransientScope();
 container.bind(UserController).toSelf().inTransientScope();
