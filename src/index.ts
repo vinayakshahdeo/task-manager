32c702e4dc9996fc0bf739e9642d86ca8f858f33
vinayakshahdeo <vinushahdeo3333@gmail.com>
2026-02-09 16:22:22 +0530

    Add CORS support and update dependencies for improved API handling

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,19 @@ import { addRoutes } from './config/routes.config';
 import mongoose from 'mongoose';
 import * as dotenv from 'dotenv';
 import { responseFormatter } from './middleware/responseFormatter';
+import cors from 'cors';
 
 const app: Express = express();
 
 dotenv.config();
+
+// let corsOptions:CorsOptions={
+// 	origin:'http://localhost:3001', //origin of the client app
+// 	methods:['GET','POST'],//methods allowed to be used in the cors request
+// 	optionsSuccessStatus:20
+// }
+
+app.use(cors());
 
 //adding middleware from express
 app.use(express.json());
